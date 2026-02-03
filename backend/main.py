36cf7b302309cb4a72a4004dd512fc9194b1e36d
kidlatpogi <dzeref4000@gmail.com>
2026-02-03 22:18:28 +0800

    Backend Ports Fixes

--- backend/main.py
+++ backend/main.py
@@ -631,7 +631,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=8000,
+        port=7860,
         reload=False,
         log_level="info"
     )
